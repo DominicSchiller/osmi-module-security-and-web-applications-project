abc3dbf2e43401b091186cec8e63f80d1cc25b90
vquynh <ng.viet.quynh@gmail.com>
2022-06-23 23:50:23 +0200

    updated optional-passwordless-required-action

--- data/keycloak/extensions/optional-passwordless-required-action/src/main/java/dominicschiller/keycloak/requireaction/OptionalPasswordlessRequiredActionFactory.java
+++ data/keycloak/extensions/optional-passwordless-required-action/src/main/java/dominicschiller/keycloak/requireaction/OptionalPasswordlessRequiredActionFactory.java
@@ -1,3 +1,5 @@
+package dominicschiller.keycloak.requireaction;
+
 import org.keycloak.Config;
 import org.keycloak.authentication.DisplayTypeRequiredActionFactory;
 import org.keycloak.authentication.RequiredActionFactory;
@@ -6,30 +8,38 @@ import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.KeycloakSessionFactory;
 
 public class OptionalPasswordlessRequiredActionFactory implements RequiredActionFactory, DisplayTypeRequiredActionFactory {
+    @Override
     public RequiredActionProvider createDisplay(KeycloakSession keycloakSession, String s) {
+        return create(keycloakSession);
+    }
+
+    @Override
+    public String getDisplayText() {
+        return "Choose Passwordless Authentication";
+    }
+
+    @Override
+    public RequiredActionProvider create(KeycloakSession keycloakSession) {
         return new OptionalPasswordlessRequiredAction();
     }
 
-    public String getDisplayText() {
-        return "Choose if you want to use password-less authentication";
-    }
 
-    public RequiredActionProvider create(KeycloakSession keycloakSession) {
-        return null;
-    }
-
+    @Override
     public void init(Config.Scope scope) {
 
     }
 
+    @Override
     public void postInit(KeycloakSessionFactory keycloakSessionFactory) {
 
     }
 
+    @Override
     public void close() {
 
     }
 
+    @Override
     public String getId() {
         return OptionalPasswordlessRequiredAction.PROVIDER_ID;
     }
--- data/keycloak/extensions/optional-passwordless-required-action/src/main/resources/theme-resources/templates/use-passwordless.ftl
+++ data/keycloak/extensions/optional-passwordless-required-action/src/main/resources/theme-resources/templates/use-passwordless.ftl
@@ -3,17 +3,10 @@
     <#if section = "header">
         ${"Choose passwordless"}
     <#elseif section = "form">
-        <h2>Choose passwordless</h2>
-        <p></p>
         <form id="kc-mobile-update-form" class="${properties.kcFormClass!}" action="${url.loginAction}" method="post">
-            <div class="${properties.kcFormGroupClass!}">
-                <div class="${properties.kcLabelWrapperClass!}">
-                    <label for="use-passwordless"class="${properties.kcLabelClass!}">Use passwordless</label>
-                </div>
-                <div class="${properties.kcInputWrapperClass!}">
-                    <input type="checkbox" id="use_passwordless" name="use_passwordless" class="${properties.kcInputClass!}"
-                           value="${use_passwordless}"/>
-                </div>
+            <div>
+                <label for="use_passwordless">Use passwordless authentication method</label>
+                <input type="checkbox" id="use_passwordless" name="use_passwordless"/>
             </div>
             <div class="${properties.kcFormGroupClass!}">
                 <div id="kc-form-buttons" class="${properties.kcFormButtonsClass!}">
