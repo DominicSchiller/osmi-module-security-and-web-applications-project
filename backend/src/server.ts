0f6a498463c037dff35e6c9d9850899ab8cb3588
Dominic <schiller.dominic@outlook.com>
2022-06-26 21:07:21 +0200

    Add cors to backend allowing requests from frontend app

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,8 +10,15 @@ const loggerMiddleware = expressPinoLogger({
     autoLogging: true,
 });
 
+const cors = require('cors')
+const corsOptions = {
+    origin: 'http://localhost:8787',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+
 // App setup
 const app: Application = express();
+app.use(cors(corsOptions))
 app.use(loggerMiddleware);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
